34e6f51f5199a3ef462b950fb4b39178566f3a37
abdi kassim <123456789+odin@users.noreply.github.com>
2026-05-21 05:25:53 +0300

    done

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears VALUES
+(1,'Mr. Chocolate',10,'M','Brown','Friendly',1);
+
+INSERT INTO bears VALUES
+(2,'Rowdy',7,'M','Black','Aggressive',1);
+
+INSERT INTO bears VALUES
+(3,'Tabitha',5,'F','White','Calm',1);
+
+INSERT INTO bears VALUES
+(4,'Sergeant Brown',12,'M','Brown','Serious',1);
+
+INSERT INTO bears VALUES
+(5,'Melissa',4,'F','Golden','Playful',1);
+
+INSERT INTO bears VALUES
+(6,'Grinch',8,'M','Green','Grumpy',1);
+
+INSERT INTO bears VALUES
+(7,'Wendy',6,'F','Black','Kind',1);
+
+INSERT INTO bears VALUES
+(8,NULL,3,'F','Brown','Quiet',1);
